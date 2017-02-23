1ad6c3ec9538fb5fa46ed8c447be3789ecb4b912
Vinicius Santana <vini@Viniciuss-MacBook-Pro.local>
2017-02-22 18:06:18 -0800

    Added mockdata, resolve function to rootquery and exported it to server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = reqire('./schema/schema');
 
 const app = express();
 
 
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true
 }));
 
